71ac88f8391042f8d223e8d7f0746ac0e724dbd3
Ayman-hussein <82322700+Ayman-hussein@users.noreply.github.com>
2022-02-25 10:22:20 +0000

    Update College.java
    getting there

--- core/src/com/mygdx/game/College.java
+++ core/src/com/mygdx/game/College.java
@@ -12,7 +12,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+//james best//
 public class College implements IHittable {
 
     // Declare config, variables
